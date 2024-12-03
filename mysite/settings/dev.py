663714be692c1c132baf71f0594b8d47a538ab58
kikoservicios <pepe@gmail.com>
2024-12-03 10:54:56 +0000

    Commit inicial

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h@*35n$f@pkhpcnle9!i37b2(2i7a=-53&3yk4wkp)^i@6u)qn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
